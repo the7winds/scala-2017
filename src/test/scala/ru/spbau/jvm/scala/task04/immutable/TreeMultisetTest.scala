86adca28e0236e1d71e3df5b3025e895f745291f
Gleb Valin <the7winds@yandex.ru>
2017-09-30 18:39:33 +0300

    add tests for immutable set

--- src/test/scala/ru/spbau/jvm/scala/task04/immutable/TreeMultisetTest.scala
+++ src/test/scala/ru/spbau/jvm/scala/task04/immutable/TreeMultisetTest.scala
@@ -1,5 +1,65 @@
 package ru.spbau.jvm.scala.task04.immutable
 
-class TreeMultisetTest {
+import org.scalatest._
 
+class TreeMultisetTest extends FlatSpec with Matchers {
+  "A multiset" should "construct from a value" in {
+    val single = TreeMultiset(42)
+    var cnt = 0
+
+    single.size should be (1)
+    single.get(42) should be (Some(1))
+  }
+
+  "A multiset" should "construct big set from sequence" in {
+    val set = TreeMultiset('3', '1', '2', '3', '2', '3')
+
+    set.get('1') should be (Some(1))
+    set.get('2') should be (Some(2))
+    set.get('3') should be (Some(3))
+  }
+
+  "A multiset" should "deconstruct in sequence" in {
+    val set = TreeMultiset('1', '2', '3')
+
+    set match {
+      case TreeMultiset(_, _, _) => ()
+    }
+  }
+
+  "Function" should "map int to string" in {
+    val intSet = TreeMultiset(1, 3, 3, 2)
+    val stringSet = intSet.map { _.toString }
+    stringSet.get("1") should be (Some(1))
+    stringSet.get("2") should be (Some(1))
+    stringSet.get("3") should be (Some(2))
+  }
+
+  "Function" should "flatmap string set to char set" in {
+    val stringSet = TreeMultiset("abba", "abba", "dabba")
+    val charSet: TreeMultiset[Char] = stringSet.flatMap {
+      v => v.foldLeft(TreeMultiset[Char]()) { (s, ch) => TreeMultiset.apply(ch, s) }
+    }
+
+    charSet.get('a') should be (Some(6))
+    charSet.get('b') should be (Some(6))
+    charSet.get('d') should be (Some(1))
+    charSet.get('c') should be (None)
+  }
+
+  it should "find element by predicate" in {
+    val stringSet = TreeMultiset("abba", "abba", "dabba", "zoom")
+    val Some(1) = stringSet.find(_ == "zoom")
+    val None = stringSet.find(_ == "boom")
+  }
+
+  "Predicate" should "filter elements" in {
+    TreeMultiset("abba", "abba", "dabba", "zoom").filter {
+      _.contains("a")
+    }.find {
+      _ == "zoom"
+    } match {
+      case None => ()
+    }
+  }
 }
